01aeb92ff40ad58713d9ab87aca43af5204ec7bd
Saurabh Joshi <saurabhsjoshi94@gmail.com>
2022-10-01 23:30:44 -0400

    [A-TEST] Row 55 a-test added

--- acceptance/src/test/java/acceptance/SinglePlayerAcceptanceTest.java
+++ acceptance/src/test/java/acceptance/SinglePlayerAcceptanceTest.java
@@ -366,10 +366,52 @@ public class SinglePlayerAcceptanceTest {
         assertEquals(200, getPlayerScore());
     }
 
-    @DisplayName("score 2 sets of 3 (monkey, swords) in RTS on first roll   (SC 300)")
+    @DisplayName("R54: score 2 sets of 3 (monkey, swords) in RTS on first roll   (SC 300)")
     @Test
     void R54() throws IOException {
         defaultRiggedCard();
+
+        // 2 sets of 3 (monkey, swords)
+        TestUtils.rigDice(reader, writer, List.of(
+                new Turn.RiggedDie(0, new Die(Die.Side.MONKEY)),
+                new Turn.RiggedDie(1, new Die(Die.Side.MONKEY)),
+                new Turn.RiggedDie(2, new Die(Die.Side.MONKEY)),
+                new Turn.RiggedDie(3, new Die(Die.Side.SWORD)),
+                new Turn.RiggedDie(4, new Die(Die.Side.SWORD)),
+                new Turn.RiggedDie(5, new Die(Die.Side.SWORD)),
+                new Turn.RiggedDie(6, new Die(Die.Side.PARROT)),
+                new Turn.RiggedDie(7, new Die(Die.Side.SKULL))
+        ));
+
+        // hold all
+        TestUtils.waitForUserPrompt(reader);
+        TestUtils.writeLine(writer, "2 0 1 2 3 4 5 6 7");
+
+        // End turn
+        TestUtils.waitForUserPrompt(reader);
+        TestUtils.writeLine(writer, "0");
+
+        assertEquals(300, getPlayerScore());
+    }
+
+    @DisplayName("R55: score 2 sets of 3 (monkey, parrots) in RTS using 2 rolls   (SC 300)")
+    @Test
+    void R55() throws IOException {
+        defaultRiggedCard();
+        TestUtils.rigDice(reader, writer, List.of(
+                new Turn.RiggedDie(0, new Die(Die.Side.GOLD_COIN)),
+                new Turn.RiggedDie(1, new Die(Die.Side.MONKEY)),
+                new Turn.RiggedDie(2, new Die(Die.Side.PARROT)),
+                new Turn.RiggedDie(3, new Die(Die.Side.SWORD)),
+                new Turn.RiggedDie(4, new Die(Die.Side.SWORD)),
+                new Turn.RiggedDie(5, new Die(Die.Side.SWORD)),
+                new Turn.RiggedDie(6, new Die(Die.Side.SKULL)),
+                new Turn.RiggedDie(7, new Die(Die.Side.SKULL))
+        ));
+
+        // re roll
+        TestUtils.waitForUserPrompt(reader);
+        TestUtils.writeLine(writer, "3");
 
         // 2 sets of 3 (monkey, swords)
         TestUtils.rigDice(reader, writer, List.of(
